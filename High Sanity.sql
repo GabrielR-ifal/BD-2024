ab16382a627564567aa940ad0536e7dfd1b9c0e6
PedrinhuDev <159052787+PedrinhuDev@users.noreply.github.com>
2024-08-16 15:55:26 -0300

    Initial commit

---
+++ High Sanity.sql
@@ -0,0 +1,70 @@
+create database trabalho;
+use trabalho;
+
+create table Postagens(
+topico varchar(15),
+titulo 	varchar(15),
+assunto varchar(45),
+id int auto_increment,
+primary key(id)
+);
+
+create table UsuarioComum(
+genero varchar(10),
+senha varchar(35),
+email varchar(30),
+nome varchar(30),
+meta varchar(75),
+foto_perfil varchar(75),
+nickname varchar(15),
+disturbio varchar(15),
+id int auto_increment,
+primary key(id)
+);
+
+create table UsuarioProfissional(
+genero varchar(10),
+senha varchar(35),
+email varchar(30),
+nome varchar(30),
+meta varchar(75),
+foto_perfil varchar(75),
+nickname varchar(15),
+id int auto_increment,
+profissao varchar(40),
+primary key(id)
+);
+
+create table Denuncias(
+data date,
+conteudo varchar(30),
+id int auto_increment,
+primary key(id),
+FK_postagens_id int,
+foreign key (FK_postagens_id) references Postagens(id)
+); 
+
+create table Comentarios(
+id int auto_increment,
+mensagem varchar(30),
+data date,
+FK_postagens_id int,
+foreign key (FK_postagens_id) references Postagens(id),
+primary key(id)
+);
+
+create table ComentariosUsuarioComum(
+FK_comentarios_id int,
+FK_usuariocomum_id int,
+foreign key(FK_comentarios_id) references Comentarios(id),
+foreign key(FK_usuariocomum_id) references UsuarioComum(id),
+primary key(FK_comentarios_id, FK_usuariocomum_id)
+);
+
+create table ComentariosUsuarioProfissional(
+FK_comentarios_id int,
+FK_usuarioprofissional_id int,
+foreign key(FK_comentarios_id) references Comentarios(id),
+foreign key(FK_usuarioprofissional_id) references UsuarioProfissional(id),
+primary key(FK_comentarios_id, FK_usuarioprofissional_id)
+);
